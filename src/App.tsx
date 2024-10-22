9583085377eb4e6addb9a1a95805462e0b749b15
narrianesil <153286088+narrianesil@users.noreply.github.com>
2024-10-22 11:44:12 -0400

    commit aula22/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type ProdutoType = {
+    id:number,
+    nome:string,
+    preco:string,
+    descricao:string,
+    imagem:string
+}
+type UsuariosType = {
+  id:number,
+  nome:string,
+  email:string,
+  created_at:string,
+  update_at:string
 }
 
+function App() {
+  const [nome, setNome] = useState("")
+  const [produtos, setProdutos] = useState<ProdutoType []>([])
+  const [usuarios, setUsuarios] = useState<UsuariosType []>([])
+
+  useEffect(() => {
+    setNome("Narriane Silva")
+    fetch("https://one022a-marketplace-aq8p.onrender.com/produtos")
+      .then(resposta => resposta.json())
+      .then(dados => setProdutos(dados))
+    }, [])
+    
+      useEffect(() => {
+      fetch("https://one022a-marketplace-aq8p.onrender.com/usuarios")
+      .then(resposta => resposta.json())
+      .then(dados => setUsuarios(dados))
+}, [])
+
+return (
+  <>
+    <h1>{nome}</h1>
+    <div className="itens-container">
+      <div className="produtos-container">
+        {produtos.map(produto => (
+          <div key={produto.id} className="produto-item">
+            <h2>{produto.nome}</h2>
+            <div className='container-imagem'>
+              <img src={produto.imagem} alt={produto.nome} />
+            </div>
+            <p>{produto.preco}</p>
+            <p>{produto.descricao}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="usuarios-container">
+        {usuarios.map(usuario => (
+          <div key={usuario.id} className="usuario-item">
+            <h2>{usuario.nome}</h2>
+            <p>{usuario.email}</p>
+            <div className='data'>
+              <p>{usuario.created_at}</p>
+              <p>{usuario.update_at}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </>
+);
+}
 export default App
+
+
+
